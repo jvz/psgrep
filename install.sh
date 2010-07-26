4fa2cb46ba878fa145a31c341c067017638acf05
boards <none@none>
2010-07-26 16:34:32 +0000

    Fixed issue 3
    --HG--
    extra : convert_revision : svn%3Aa4405340-9a3f-0410-a3d8-31b31f83d5c0/trunk%4020

--- install.sh
+++ install.sh
@@ -4,13 +4,22 @@ usage()
     echo "Usage: $0
 
 To change the prefix, set the PREFIX variable before running; e.g.
-PREFIX=/usr $0
+env PREFIX=/usr sudo $0
 
-The default PREFIX is /usr/local"
+The default PREFIX is /usr/local
+Other suggested PREFIXes include:
+${HOME}/.local
+/usr
+/opt/psgrep"
     exit 1
 }
 
 [ $# -ge 1 ] && usage
 
-install -D -m755 psgrep ${PREFIX:-/usr/local}/bin/psgrep
-install -D -m644 psgrep.1 ${PREFIX:-/usr/local}/share/man/man1/psgrep.1
+PREFIX="${PREFIX:-/usr/local}"
+
+# fixes issue 3: non-GNU install(1) doesn't support -D
+install -d "${PREFIX}/bin"
+install -d "${PREFIX}/share/man/man1"
+install -m755 psgrep ${PREFIX:-/usr/local}/bin/psgrep
+install -m644 psgrep.1 ${PREFIX:-/usr/local}/share/man/man1/psgrep.1
